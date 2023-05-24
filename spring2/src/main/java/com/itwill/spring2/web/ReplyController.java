ccd093552df921e985633531be94dd6f67b239e3
kkwh <121307297+kkwh@users.noreply.github.com>
2023-05-24 19:52:02 +0900

    Add files via upload

--- spring2/src/main/java/com/itwill/spring2/web/ReplyController.java
+++ spring2/src/main/java/com/itwill/spring2/web/ReplyController.java
@@ -1,13 +1,19 @@
 package com.itwill.spring2.web;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itwill.spring2.domain.Reply;
 import com.itwill.spring2.dto.ReplyCreateDto;
+import com.itwill.spring2.dto.ReplyReadDto;
 import com.itwill.spring2.service.ReplyService;
 
 import lombok.AllArgsConstructor;
@@ -32,16 +38,25 @@ public class ReplyController {
         return ResponseEntity.ok(result);
     }
     
-//    @AllArgsConstructor @Data
-//    public class Test{
-//        private int age;
-//        private String name;
-//        
-//    }
-//    @GetMapping
-//    public Test hello() {
-//        log.info("hello");
-//        return new Test(16, "오쌤");
-//    }
+    @GetMapping("/all/{postId}")
+    public ResponseEntity<List<ReplyReadDto>> read(@PathVariable long postId) {
+        log.info("read(postId={})", postId);
+        
+        List<ReplyReadDto> list = replyService.read(postId);
+        log.info("# of replies = {}", list.size());
+        
+        return ResponseEntity.ok(list);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Integer> deleteReply(@PathVariable long id) {
+        log.info("deleteReply(id={})", id);
+        
+        int result = replyService.delete(id);
+        
+        return ResponseEntity.ok(result);
+    }
+    
+
     
 }
